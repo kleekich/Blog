19a6200008b3311d14144447ab0a737365165a12
Kangsik Kevin Lee <kangsik.kevin.lee@gmail.com>
2018-03-28 21:48:47 +0000

    comments for indicating code blocks added, data schema added, model created using the schema

--- app.js
+++ app.js
@@ -3,11 +3,25 @@ var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 
+// App Config
 mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
+
+// Mongoose/Model Config
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+
+// RESTful Routes 
 app.get("/", function(req, res){
     res.send("this is landing page");
 })
