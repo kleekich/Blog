12175a1e2c6a99bf9d3b03c193054788985c799f
Kangsik Kevin Lee <kangsik.kevin.lee@gmail.com>
2018-03-28 22:01:23 +0000

    dummy data(commented) added to our db and our views directory, index.ejs file created

--- app.js
+++ app.js
@@ -20,10 +20,20 @@ var blogSchema = new mongoose.Schema({
 
 var Blog = mongoose.model("Blog", blogSchema);
 
+// Blog.create({
+//     title: "Introducing my dog, Yppi!",
+//     image: "https://images.unsplash.com/photo-1436658040953-a21ef6596481?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=4cf2ea5d08cef2d24d85985bee63628f&auto=format&fit=crop&w=800&q=60",
+//     body: "He's name is Yppi, and he is 14-year-old dog. He is shy little boy.",
+    
+// })
 
 // RESTful Routes 
-app.get("/", function(req, res){
-    res.send("this is landing page");
+app.get("/blogs", function(req, res){
+    res.render("index");
+})
+
+app.get("/blogs", function(req, res){
+    res.render
 })
 
 app.listen(process.env.PORT, process.env.IP, function(){
